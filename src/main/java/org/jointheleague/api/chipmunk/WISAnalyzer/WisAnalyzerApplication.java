e6ba199aa94b5ea75af2b045ec7208a2d0e0ca46
amoizhao <zhaoa6125@gmail.com>
2022-02-05 16:37:59 -0800

    fixed error

--- src/main/java/org/jointheleague/api/chipmunk/WISAnalyzer/WisAnalyzerApplication.java
+++ src/main/java/org/jointheleague/api/chipmunk/WISAnalyzer/WisAnalyzerApplication.java
@@ -2,8 +2,11 @@ package org.jointheleague.api.chipmunk.WISAnalyzer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+
 public class WisAnalyzerApplication {
 
 	public static void main(String[] args) {
