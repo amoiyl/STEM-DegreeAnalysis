e30ac7d8b171432ce824a32a7e80e9e73b2ab259
amoizhao <zhaoa6125@gmail.com>
2022-02-26 11:27:41 -0800

    milestone 7

--- src/main/java/org/jointheleague/api/level7/chipmunk/WISAnalyzer/repository/ACSRepository.java
+++ src/main/java/org/jointheleague/api/level7/chipmunk/WISAnalyzer/repository/ACSRepository.java
@@ -1,9 +1,27 @@
 package org.jointheleague.api.level7.chipmunk.WISAnalyzer.repository;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.web.reactive.function.client.WebClient;
 
 public class ACSRepository {
+    private String baseURL = "https://api.census.gov/data/2019/acs/acs1/groups/B15012/";
+    private final WebClient webClient;
+
+    public ACSRepository() {
+        webClient = WebClient
+                .builder()
+                .baseUrl(baseURL)
+                .build();
+    }
+
     public String getResults(String query){
-        return "Searching for the " + query + " STEM population.";
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .queryParam("", "")
+                        .build()
+                )
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
     }
 }
