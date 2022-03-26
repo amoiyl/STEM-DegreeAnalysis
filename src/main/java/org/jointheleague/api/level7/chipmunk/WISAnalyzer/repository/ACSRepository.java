0b0a86b41b8300bbae1004a93573ccaa6bbccbe9
amoizhao <zhaoa6125@gmail.com>
2022-03-26 11:26:27 -0700

    add url

--- src/main/java/org/jointheleague/api/level7/chipmunk/WISAnalyzer/repository/ACSRepository.java
+++ src/main/java/org/jointheleague/api/level7/chipmunk/WISAnalyzer/repository/ACSRepository.java
@@ -4,7 +4,8 @@ import org.springframework.stereotype.Repository;
 import org.springframework.web.reactive.function.client.WebClient;
 
 public class ACSRepository {
-    private String baseURL = "https://api.census.gov/data/2019/acs/acs1/groups/B15012/";
+    // private String baseURL = "https://api.census.gov/data/2019/acs/acs1/groups/B15012/?get=B15012_008MA";
+    private String baseURL = "http://api.census.gov/data/2018/acs/acs1/subject?get=group(S1502)&for=state:*";
     private final WebClient webClient;
 
     public ACSRepository() {
@@ -16,10 +17,6 @@ public class ACSRepository {
 
     public String getResults(String query){
         return webClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .queryParam("", "")
-                        .build()
-                )
                 .retrieve()
                 .bodyToMono(String.class)
                 .block();
