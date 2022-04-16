2b5b01b7144545ce2a37128fab5e7596baafcfb5
amoizhao <zhaoa6125@gmail.com>
2022-04-16 16:44:09 -0700

    added json schema

---
+++ src/main/java/org/jointheleague/api/level7/chipmunk/WISAnalyzer/repository/dto/ACSResponse.java
@@ -0,0 +1,27 @@
+package org.jointheleague.api.level7.chipmunk.WISAnalyzer.repository.dto;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+public class ACSResponse {
+
+    private List<Result> results = null;
+
+    @JsonProperty("results")
+    public List<Result> getResults() {
+        return results;
+    }
+
+    @JsonProperty("results")
+    public void setResults(List<Result> results) {
+        this.results = results;
+    }
+
+}
